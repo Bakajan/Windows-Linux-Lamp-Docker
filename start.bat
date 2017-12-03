c21630668a9483e2df95119cf9afd6f9fccd142f
Bakajan <omega3mini@yahoo.com>
2017-12-03 17:45:47 -0500

    Added Dockerfile and shell/batch scripts

---
+++ start.bat
@@ -0,0 +1,2 @@
+for %%I in (.) do set curr=%%~sI
+docker run  -d -p 8080:80 -p 8443:443 -p 8022:22 -v %curr%\html:/var/www/html -v %curr%\database:/var/lib/phpMyAdmin/upload -t otherdata/centos-docker-lamp:latest
---
+++ start.sh
@@ -0,0 +1,1 @@
+docker run -d --name lamp -p 8080:80 -p 8443:443 -p 8022:22 -v "$PWD"/html:/var/www/html -v "$PWD"/database:/var/lib/phpMyAdmin/upload -t otherdata/centos-docker-lamp:latest
