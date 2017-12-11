1a9edb2183c4fef3399325a4f5c9575eb6e9a6c3
Bakajan <omega3mini@yahoo.com>
2017-12-10 21:28:05 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,6 @@
 for %%I in (.) do set curr=%%~sI
-docker run  -d -p 8080:80 -p 8443:443 -p 8022:22 -v %curr%\html:/var/www/html -v %curr%\database:/var/lib/phpMyAdmin/upload -t otherdata/centos-docker-lamp:latest
+docker run -d -p 8080:80 -p 8443:443 -p 8022:22 ^
+-v %curr%\html:/var/www/html ^
+-v %curr%\database:/var/lib/phpMyAdmin/upload ^
+-t otherdata/centos-docker-lamp:latest ^
+--name lamp
