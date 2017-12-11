2f4e806e395bedb5c4e8abbf84bc1d2961d68075
Bakajan <omega3mini@yahoo.com>
2017-12-10 21:29:36 -0500

    Added new lines for easier reading; added image name

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-docker run -d --name lamp -p 8080:80 -p 8443:443 -p 8022:22 -v "$PWD"/html:/var/www/html -v "$PWD"/database:/var/lib/phpMyAdmin/upload -t otherdata/centos-docker-lamp:latest
+docker run -d --name lamp -p 8080:80 -p 8443:443 -p 8022:22 \
+-v "$PWD"/html:/var/www/html \
+-v "$PWD"/database:/var/lib/phpMyAdmin/upload  \
+-t otherdata/centos-docker-lamp:latest
